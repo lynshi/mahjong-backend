baea92cb98cba87fb05d9087260c15600e7a9421
Lyndon Shi <shilyndon@gmail.com>
2020-08-16 00:21:56 -0700

    function for retrieving connection

---
+++ negotiate/__init__.py
@@ -0,0 +1,13 @@
+import logging
+import azure.functions as func
+
+
+# pylint: disable=unused-argument
+def main(req: func.HttpRequest, connectionInfoJson: str) -> func.HttpResponse:
+    return func.HttpResponse(
+        connectionInfoJson,
+        status_code=200,
+        headers={
+            'Content-type': 'application/json'
+        }
+    )
